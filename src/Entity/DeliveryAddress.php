fe75513c6a85a6861da1a79ff567e74573cffb4d
Stefan Doorn <stefan@efectos.nl>
2017-06-11 21:12:28 +0200

    Add DeliveryAddress entity

---
+++ src/Entity/DeliveryAddress.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace BecosoftApi\Entity;
+
+/**
+ * Class DeliveryAddress
+ * @package BecosoftApi\Entity
+ */
+class DeliveryAddress extends AbstractEntity implements EntityInterface
+{
+    protected static $endpoint = 'DeliveryAddress';
+}
