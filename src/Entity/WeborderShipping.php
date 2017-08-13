3c47a35f97580c66d4e53ae32d3aa689d6851ea4
Stefan Doorn <stefan@efectos.nl>
2017-08-13 12:06:16 +0200

    Add entity WeborderShipment

---
+++ src/Entity/WeborderShipping.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BecosoftApi\Entity;
+
+/**
+ * Class WeborderShipping
+ * @package BecosoftApi\Entity
+ */
+class WeborderShipping extends AbstractEntity implements EntityInterface
+{
+    protected static $endpoint = 'WeborderShipping';
+    protected static $idField = 'WeborderId';
+}
